7053c7421ad7f3bbf390f1665a5a33da4f8b81c6
jdegenfellner <jdegenfellner@gmail.com>
2025-06-04 16:18:34 +0200

    up

---
+++ Solutions_Exercises/3_LMM/Exercise_11.R
@@ -0,0 +1,4 @@
+# ex 11 simple log reg
+
+# logit(p) = g(p) = β0 + β1x1
+# Hence g^{-1}(.) = inv_logit(.)
---
+++ Solutions_Exercises/3_LMM/Untitled.R
@@ -0,0 +1,5 @@
+#ex 11 log reg
+
+# logit(p) = g(p) = β0 + β1x1
+
+# Hence g^{-1}(.) = inv_logit(.)
